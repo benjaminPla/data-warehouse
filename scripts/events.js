1ff5ae304aefd6ef4be945a40bc79526b3cf16e2
LTSFben <ltsf.benja@gmail.com>
2021-09-13 12:11:07 -0300

    cleaning code && working on pop respones

--- scripts/events.js
+++ scripts/events.js
@@ -15,28 +15,40 @@ const events = {
   },
   postAreaBtn: () => {
     document.getElementById("post_area-btn").addEventListener("click", () => {
-      functions.fillNode("section-main", dom.pop());
+      functions.fillNode("section-main", dom.pop);
       events.closePop();
-      functions.fillNode("pop-container", dom.areaPost());
+      functions.fillNode("pop-container", dom.areaPost);
       events.areaPostSaveBtn();
     });
   },
   closePop: () => {
-    document.getElementById("pop").addEventListener("click", (element) => {
+    document.getElementById("pop").addEventListener("mousedown", (element) => {
       if (element.target.id === "pop") document.getElementById("pop").remove();
     });
   },
   areaPostSaveBtn: () => {
     document.getElementById("area_save-btn").addEventListener("click", async () => {
-      const body = { name: document.getElementById("area_name-post-input").value, countryId: 1 }; //cambiar el countryId
+      const body = {
+        name: document.getElementById("area_name-post-input").value,
+        countryId: document.getElementById("country_name-post-input").value, //cambiar el countryId por countryName || mejor un <select>
+      };
       const response = await functions.fetchPost("http://localhost:3000/cities/post", body);
-      functions.clearNode("post-response");
-      functions.fillNode("post-response", response.response);
       if (response.success) {
+        functions.clearNode("response");
+        document.getElementById("response").classList.remove("response-error");
+        document.getElementById("response").classList.add("response-success");
+        functions.fillNode("response", dom.successIcon);
+        functions.fillNode("response", response.response);
         setTimeout(() => {
           document.getElementById("pop").remove();
           document.getElementById("areas-btn").click();
         }, 1000);
+      } else {
+        functions.clearNode("response");
+        document.getElementById("response").classList.remove("response-success");
+        document.getElementById("response").classList.add("response-error");
+        functions.fillNode("response", dom.errorIcon);
+        functions.fillNode("response", response.response);
       }
     });
   },
