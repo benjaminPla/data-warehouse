86abaea69645eb8a4627c88ad2ef11a6e88f1f87
benjaminPla <benjaminpla.dev@gmail.com>
2021-09-14 12:53:50 -0300

    add object response [cities, countries]

--- src/controllers/countries.js
+++ src/controllers/countries.js
@@ -1,24 +1,31 @@
 import { sequelize } from "../../server/server.js";
 import { countries } from "../../server/countries.js";
 
+let response = { success: true, body: null };
+
 const countriesControllers = {
   findAll: async (req, res) => {
-    res.send(await countries.findAll());
+    response.body = await countries.findAll();
+    res.send(response);
   },
   findByName: async (req, res) => {
-    res.send(await countries.findByName(req.body.name));
+    response.body = await countries.findByName(req.body.name);
+    res.send(response);
   },
   post: (req, res) => {
+    response.body = `posted ${req.body.name}`;
     countries.post(req.body);
-    res.send(`posted ${req.body.name}`);
+    res.send(response);
   },
   delete: (req, res) => {
+    response.body = `deleted ${req.body.name}`;
     countries.delete(req.body.name);
-    res.send(`deleted ${req.body.name}`);
+    res.send(response);
   },
   put: (req, res) => {
+    response.body = `updated ${req.body.name} to ${req.body.newName}`;
     countries.put(req.body);
-    res.send(`updated ${req.body.name} to ${req.body.newName}`);
+    res.send(response);
   },
 };
 
--- src/middlewares/countries.js
+++ src/middlewares/countries.js
@@ -1,30 +1,31 @@
 import { countries } from "../../server/countries.js";
 import { regions } from "../../server/regions.js";
 
+let response = { success: false, body: null };
+
 const countriesMiddlewares = {
   countryNotFound: async (req, res, next) => {
-    (await countries.findByName(req.body.name)) == ""
-      ? res.status(400).send("country not found")
-      : next();
+    response.body = "country not found";
+    (await countries.findByName(req.body.name)) == "" ? res.send(response) : next();
   },
   nameAlreadyExists: async (req, res, next) => {
-    (await countries.findByName(req.body.name)) == ""
-      ? next()
-      : res.status(400).send("country already exists");
+    (await countries.findByName(req.body.name)) == "" ? next() : res.send(response);
   },
   missingName: (req, res, next) => {
-    !req.body.name ? res.status(400).send("missing name") : next();
+    response.body = "missing name";
+    !req.body.name ? res.send(response) : next();
   },
   missingNewName: (req, res, next) => {
-    !req.body.newName ? res.status(400).send("missing newName") : next();
+    response.body = "missing newName";
+    !req.body.newName ? res.send(response) : next();
   },
   missingRegionId: (req, res, next) => {
-    !req.body.regionId ? res.status(400).send("missing regionId") : next();
+    response.body = "missing regionId";
+    !req.body.regionId ? res.send(response) : next();
   },
   regionNotFound: async (req, res, next) => {
-    (await regions.findById(req.body.regionId)) == ""
-      ? res.status(400).send("region not found")
-      : next();
+    response.body = "region not found";
+    (await regions.findById(req.body.regionId)) == "" ? res.send(response) : next();
   },
 };
 
